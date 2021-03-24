bfe546543a93d2334b9e4359b965e2dde6c9c8c7
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-24 15:57:34 +0100

    inserito saluto e immagine nella pagina tramite vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.*/
+/* Stampare a schermo un messaggio all’interno di un h1, utilizzando i data. + bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
+var app = new Vue({
+  el: '#box',
+  data: {
+    message: 'Ciao Visitatore',
+    greeting: 'Benvenuto in questa pagina nostalgica',
+    img: 'img/sfondowindowsxp.jpg',
+  }
+});
